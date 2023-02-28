4ec057ac7f04c223d5fc0c5909e92f0f5e648752
mia336love <122865270+mia336love@users.noreply.github.com>
2023-02-28 20:55:42 +0300

    calculate done and tested

--- calculate.js
+++ calculate.js
@@ -1,12 +1,11 @@
-const cartAmount = 16554;
-const numOfItems = 5;
+const cartAmount = 56000;
+const numOfItems = 12;
 const promoCode = null;
 
-discountCartAmount = cartAmount;
-
 function calculate(cartAmount, numOfItems, promoCode) {
+  discountCartAmount = cartAmount;
   if (promoCode === "ДАРИМ300") {
-    discountCartAmount = cartAmount - 300;
+    discountCartAmount = discountCartAmount - 300;
   } else if (promoCode === "ДАРИМ300" && cartAmount < 300) {
     discountCartAmount = 0;
   }
@@ -17,29 +16,10 @@ function calculate(cartAmount, numOfItems, promoCode) {
     const difference = discountCartAmount - 50000;
     const discount = (difference / 100) * 20;
     discountCartAmount = difference - discount;
-    //discountCartAmount = cartAmount - ((cartAmount - 50000) / 100) * 20;
   }
   if (promoCode === "СКИДКА15" && discountCartAmount >= 20000) {
     discountCartAmount = discountCartAmount - (discountCartAmount / 100) * 15;
   }
   console.log("Стоимость: ", discountCartAmount);
 }
-
-/*
-function calculate(cartAmount, numOfItems, promoCode) {
-    for (const discountCartAmount = 0; discountCartAmount++) {
-        if (promoCode === "ДАРИМ300") {
-            cartAmount - 300; //прописать ограничение для 0
-            discountCartAmount++
-        }
-        if (numOfItems >= 10) {
-            cartAmount = cartAmount - (cartAmount / 100) * 5;
-            discountCartAmount++
-        }
-        if (cartAmount > 50000) {
-            cartAmount = cartAmount - ((cartAmount - 50000) / 100) * 20;
-            discountCartAmount++
-        }
-    }
-}
-*/
+calculate(cartAmount, numOfItems, promoCode);
