9c5304a12310714ab77fa7d1e780bc9f4669a0fe
mia336love <122865270+mia336love@users.noreply.github.com>
2023-02-28 19:09:30 +0300

    add emails

---
+++ emails.js
@@ -0,0 +1,13 @@
+const allEmails = [
+  "aaaa@gmail.com",
+  "MikleScott@gmail.com",
+  "kissmeagain@gmail.com",
+  "bigBoo@gmail.com",
+  "ensemlestar@gmail.com",
+  "speedspeedlover@gmail.com",
+];
+const blackList = [
+  "MikleScott@gmail.com",
+  "bigBoo@gmail.com",
+  "ensemlestar@gmail.com",
+];
