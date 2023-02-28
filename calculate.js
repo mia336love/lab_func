b861387eac3afbda9cbfc2f10d070665955f0484
mia336love <122865270+mia336love@users.noreply.github.com>
2023-02-28 20:07:35 +0300

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -2,6 +2,19 @@ const cartAmount = 16554;
 const numOfItems = 5;
 const promoCode = null;
 
+discountCartAmount;
+/*
 function calculate(cartAmount, numOfItems, promoCode) {
-    switch
+  if (promoCode === "ДАРИМ300") {
+    cartAmount - 300; //прописать ограничение для 0
+  }
+  if (numOfItems >= 10) {
+    cartAmount = cartAmount - (cartAmount / 100) * 5;
+  }
+  if (cartAmount > 50000) {
+    cartAmount = cartAmount - ((cartAmount - 50000) / 100) * 20;
+  }
+  if (promoCode === "СКИДКА15") {
+  }
 }
+*/
